c9368a3519c37fc49c104f1f3af7f35a3d3e896e
James Allen <jamesba555@yahoo.com>
2024-07-17 13:31:56 -0500

    wip1

---
+++ WorkingWithCSVFiles.py
@@ -0,0 +1,20 @@
+# Working with CSV files
+
+# The CSV Format:
+
+# Comma-Separated Values
+# Data is stored in a flat format as rows of vlaues, separated by commas
+# Widely used as common storage format for beneric spreadsheet data
+# Useful for transfer of data between databases and spreadsheets
+# Ex.
+#       Ticker, Company, Open, Close
+#       GOOG, Alphabet, 147.89, 149.21
+#       META, Meta Platforms, 421.78, 456.23
+#       MSFT, Microsoft Copr., 419.03, 403.75
+#       TSLA, Tesla Inc., 173.84, 171.09
+#       AMZN, Amazon.com Inc., 179.01, 175.24
+
+
+
+# Reading CSV files into an array:
+
