46a1932c0a3e38f460e7b22e0633e0875c51b922
James Allen <jamesba555@yahoo.com>
2024-07-17 13:23:02 -0500

    done?

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,11 @@
-# Introduction
+# Introduction:
+
+
+# Spreadsheets are used for everything in business today
+# You can use Python to create spreadsheets, extract data, and automate just about any workflow
+# Will be able to build sophisticated automations 
+
+# Ran pip install xlsxwriter in terminal
+# Ran pip install openpyxl in terminal
+# Ran pip install padas in terminal
+
