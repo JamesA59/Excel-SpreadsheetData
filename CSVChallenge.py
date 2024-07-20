473aff6ef1005d530cd506d851dd36b849c8436b
James Allen <jamesba555@yahoo.com>
2024-07-20 15:20:28 -0500

    done

--- CSVChallenge.py
+++ CSVChallenge.py
@@ -2,31 +2,37 @@
 
 import csv
 import pprint
+from decimal import Decimal
 
-def read_csv_to_dict(filename):
-    data = {}
+def read_csv_to_array(filename):
+    data = []
     with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        key = 0
+        reader = csv.reader(csvfile)
         for row in reader:
-            data[key] = row
-            key +=1
+            data.append(row)
     return data
 
-inventory_data = read_csv_to_dict("Inventory.csv")
-'''
-# Accessing data
-# pprint is pretty print
+
+
+
+
+def write_array_to_csv(data, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+
+
+inventory_data = read_csv_to_array("Inventory.csv")
+#pprint.pprint(inventory_data)
+
+headers = inventory_data[0]
+headers.append("Margin")
+
+datarows = inventory_data[1:]
+for row in datarows:
+    margin_value = Decimal(row[4]) - Decimal(row[3])
+    row.append(margin_value)
+
 pprint.pprint(inventory_data)
 
-# To this:
-pprint.pprint(inventory_data[0])
-pprint.pprint(inventory_data[0]["Consumer Price"])
-
-# Each item is now differentiated by a number instead of it's name, but is still essentially the same
-'''
-
-data = [inventory_data[0]]
-fieldnames= ["Item Name", "Category", "Quantity", "Wholesale Price", "Consumer Price", "Margin" ]
-
-print(data)
+write_array_to_csv(inventory_data, "CSVChallengeoutput.csv")
