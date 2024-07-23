7cb0f8582f56039fcc8741c5f8a44e6910245a7d
James Allen <jamesba555@yahoo.com>
2024-07-23 14:22:45 -0500

    wip5

--- ManipulatingExcelFilesWithOpenpxyl.py
+++ ManipulatingExcelFilesWithOpenpxyl.py
@@ -58,6 +58,7 @@ for worksheet_name in workbook.sheetnames:
 
 # Create a new workbook with worksheets and add content 
 
+'''
 from openpyxl import Workbook
 import datetime
 import random
@@ -90,3 +91,107 @@ sheet2.append(["One", "Two", "Three"])
 # Save the workbook - values don't update until we do this!
 wb.save("NewWorkbook.xlsx")
 print("Workbook has been created successfully!")
+'''
+
+
+# Working with Content:
+
+# Can get an entire column or row of cells by using either a letter or numberic index recpecivly
+
+'''
+import openpyxl
+from openpyxl.comments import Comment
+from collections import defaultdict
+
+
+# Create a new workbook
+filename = "FinancialSample.xlsx"
+
+# Load the workbook
+wb = openpyxl.load_workbook(filename)
+
+# Get the active worksheet
+sheet = wb.active
+
+# Get entire column or row of cells
+col = sheet["C"]
+row = sheet[10]
+print(f"{len(col)} cells in a column")
+print(f"{len(row)} cells in a row")
+
+# Get a range of cells
+range = sheet["A2:B7"]
+print(f"{len(range)} cells in range")
+print(range)
+
+# iterate over rows and columns
+for col in sheet.iter_cols(min_row=2, max_row=3, min_col=2, max_col=5):
+    for cell in col:
+        print(cell.value)
+counter = defaultdict(int)
+for row in sheet.iter_rows(min_row=2, min_col=3, max_col=3):
+    for cell in row:
+        counter[cell.value] +=1
+print(counter)
+
+# create a cell with a comment in it
+cell = sheet["A1"]
+cell.comment = Comment("This is a comment", "James Allen")
+
+# save the workbook
+wb.save("Content.xlsx")
+'''
+
+
+# Styling Cells:
+
+# Manipulate cell content and styling 
+
+from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment, Border, Side
+import openpyxl.styles.numbers as opnumstyle
+import datetime
+
+
+# Create a new workbook
+wb = Workbook()
+
+# Get the active worksheet and name it "TestSheet"
+sheet = wb.active
+sheet.title = "First"
+
+# Add some data to the new sheet
+sheet["A1"] = "Test Data"
+sheet["B1"] = 123.4567
+sheet["C1"] = datetime.datetime(2030, 4, 1)
+
+# Inspect the default styles of each cell
+print(sheet["A1"].style)
+print(sheet["B1"].number_format)
+print(sheet["C1"].number_format)
+
+# Use some built-in styles
+# Moving on from these to custom syles down below
+#sheet["A1"].style = "Title"
+#sheet["B1"].style = "Calculation"
+#sheet["C1"].style = "Accent2"
+#sheet["B1"].number_format = opnumstyle.FORMAT_CURRENCY_USD_SIMPLE
+#sheet["C1"].number_format = opnumstyle.FORMAT_DATE_DDMMYY
+
+sheet.column_dimensions['A'].width = 30
+sheet.row_dimensions[1].height = 50
+
+# Create styles using Fonts and Colors
+italic_font = Font(italic=True, size=16)
+colored_text = Font(name="Courier New", size=20, color="000000FF")
+centered_text = Alignment(horizontal="center", vertical="top")
+border_side = Side(border_style="mediumDashed")
+cell_border = Border(top=border_side, right=border_side, left=border_side, bottom=border_side)
+
+sheet["A1"].font = italic_font
+sheet["B1"].font = colored_text
+sheet["B1"].alignment = centered_text
+sheet["c1"].border = cell_border
+
+# Save the workbook
+wb.save("StyledCells.xlsx")
