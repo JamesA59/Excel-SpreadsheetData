2a3bcd123a13342b0e9b7d6b36cc41606909ac46
James Allen <jamesba555@yahoo.com>
2024-07-29 18:21:36 -0500

    more wip

--- ManipulatingExcelFilesWithOpenpxylChallenge.py
+++ ManipulatingExcelFilesWithOpenpxylChallenge.py
@@ -3,11 +3,13 @@
 # Split a single worksheet into multiple worksheets
 
 import openpyxl
+from openpyxl.styles import Font, Alignment, Border, Side
+import pandas as pd
 
 filename = "FinancialSample.xlsx"
-wb = openpyxl.load_workbook(filename)
+workbook = openpyxl.load_workbook(filename)
 
-sheet = wb.active
+sheet = workbook.active
 column = sheet["B"]
 all = [column[x].value for x in range(len(column))]
 countries = list(set(all))
@@ -18,15 +20,38 @@ row = sheet[1]
 header = [row[x].value for x in range (len(row))]
 print(header)
 
+for worksheet_name in workbook.sheetnames:
+    worksheet = workbook[worksheet_name]
+    dimensions = workbook.dimensions
+    print(f"Min row: {worksheet.min_row}")
+
+
 
 y = 3
 n = str(y)
 p = 0
 new = "sheet" + n
+#t = 2
+#rows = sheet[t]
+
 
 for i in range(len(countries)):
-    new = wb.create_sheet(countries[p])
+    new = workbook.create_sheet(countries[p])
     new.append(header)
+    #sheet["A1:P1"].style = "Accent 2"
+    t = 2
+    rows = sheet[t]
+    '''
+    if rows[1].value == countries[p]:
+        data = [rows[x].value for x in range (len(rows))]
+        new.append(data)
+        t += 1
+    else:
+        t += 1
+    '''
+    #for i in range(len())
+
+
     y += 1
     p += 1
 
