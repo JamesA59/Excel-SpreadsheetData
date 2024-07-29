64030bf1bb6332936291257b19d034a24dce1165
James Allen <jamesba555@yahoo.com>
2024-07-29 16:30:25 -0500

    wip2

--- ManipulatingExcelFilesWithOpenpxylChallenge.py
+++ ManipulatingExcelFilesWithOpenpxylChallenge.py
@@ -4,3 +4,36 @@
 
 import openpyxl
 
+filename = "FinancialSample.xlsx"
+wb = openpyxl.load_workbook(filename)
+
+sheet = wb.active
+column = sheet["B"]
+all = [column[x].value for x in range(len(column))]
+countries = list(set(all))
+countries.sort()
+countries.__delitem__(1)
+
+row = sheet[1]
+header = [row[x].value for x in range (len(row))]
+print(header)
+
+
+sheet3 = wb.create_sheet(countries[0])
+sheet3.append(header)
+
+sheet4 = wb.create_sheet(countries[1])
+sheet4.append(header)
+
+sheet5 = wb.create_sheet(countries[2])
+sheet5.append(header)
+
+sheet6 = wb.create_sheet(countries[3])
+sheet6.append(header)
+
+sheet7 = wb.create_sheet(countries[4])
+sheet7.append(header)
+
+wb.save("newFinancialSample.xlsx")
+print("Workbook created successfully!")
+print(countries)
