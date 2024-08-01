bae843ae9b05bdca853ed0e50d3b242f26ca43b6
James Allen <jamesba555@yahoo.com>
2024-08-01 15:50:53 -0500

    wip1

--- CreatingExcelFilesWithXLSXWriterChallenge.py
+++ CreatingExcelFilesWithXLSXWriterChallenge.py
@@ -1,1 +1,11 @@
 # Creating Excel Files With XLSX Writer Challenge:
+
+# Read a single CSV file and split it into multiple worksheets
+
+import csv
+import xlsxwriter
+
+
+filename = "Inventory.csv"
+
+# PUT YOUR CHALLENGE CODE HERE
